794fac766b05ab17e58e7554a3dadedcc59e98fe
Konrad <konrad.kipczak@gmail.com>
2018-01-22 17:02:38 +0000

    added functionality to downloader

--- picDownloader.js
+++ picDownloader.js
@@ -1,11 +1,17 @@
 var request = require('request');
 var fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples')               
+request.get('https://sytantris.github.io/http-examples/future.jpg')               
        .on('error', function (err) {                                  
          throw err; 
        })
        .on('response', function (response) {                           
-         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Code: ', response.statusMessage, '\nContent Type:', response.headers['content-type']);
+       })
+       .on('data', function (response) {                           
+         console.log('Downloading Content...');
+       })
+       .on('end', function (response) {                           
+         console.log('Download Complete!');
        })
        .pipe(fs.createWriteStream('./future.jpg'));               
